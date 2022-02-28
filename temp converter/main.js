b93b1d2ed0261d55f0e38905a41d968219f51041
itznik2511 <98873864+itznik2511@users.noreply.github.com>
2022-03-01 00:06:59 +0530

    Add files via upload

---
+++ temp converter/main.js
@@ -0,0 +1,14 @@
+let celsius = document.getElementById("celsius");
+let fahrenheit = document.getElementById("fahrenheit");
+//console.log(celsius,fahrenheit);
+function celTofah() {
+    let output = (parseFloat(celsius.value) * 9 / 5) + 32;
+    fahrenheit.value = parseFloat(output.toFixed(2));
+    //  console.log(output);
+}
+
+function fahTocel() {
+    let output = (parseFloat(fahrenheit.value) - 32) * 5 / 9;
+    celsius.value = parseFloat(output.toFixed(2));
+    //console.log(output);
+}
